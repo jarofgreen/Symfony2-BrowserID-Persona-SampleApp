153fabb3792a3de551d7ad861809be2063f29ec8
James <james@jarofgreen.co.uk>
2012-09-29 20:37:13 +0100

    we can verify assertions

--- src/JarOfGreen/BrowserIDBundle/Controller/DefaultController.php
+++ src/JarOfGreen/BrowserIDBundle/Controller/DefaultController.php
@@ -3,12 +3,36 @@
 namespace JarOfGreen\BrowserIDBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
 	
-    public function loginAction()
+    public function loginAction(Request $request)
     {
+		
+		$assertion = $request->request->get('assertion');
+		if ($assertion) {
+			
+			$audience = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'];
+			$postdata = 'assertion=' . urlencode($assertion) . '&audience=' . urlencode($audience);
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, "https://verifier.login.persona.org/verify");
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+			$json = curl_exec($ch);
+			curl_close($ch);
+			
+			$json = json_decode($json);
+			if (is_object($json) && isset($json->status) && $json->status == "okay") {
+				print "EX";
+				die();
+			}
+			
+		}
+		
         return $this->render('JarOfGreenBrowserIDBundle:Default:login.html.twig', array());
     }
 
